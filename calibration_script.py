2c3ed94dc251a69ebda7257e5f9214c9c94ff705
Chris Salem <chris@Chris-11-Air.attlocal.net>
2025-12-04 00:12:22 -0800

    fix another error

--- calibration_script.py
+++ calibration_script.py
@@ -15,7 +15,7 @@ warnings.filterwarnings("ignore", category=DeprecationWarning)
 # ==============================
 cmd = "govee2mqtt/target/debug/govee serve --govee-email hebardiansbehardians@gmail.com --govee-password 777Markofthebeast!"
 PUMP_IP = "192.168.1.65" 
-HEATER_IP = "192.168..64"
+HEATER_IP = "192.168.1.64"
 
 CHECK_INTERVAL = 60       # check every 1 minute
 LOG_INTERVAL = 600        # record every 10 minutes when conditions are met
