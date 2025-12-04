c0fcfd4b01d41b36840817a9a73d736e1bea8817
Chris Salem <chris@Chris-11-Air.attlocal.net>
2025-12-03 23:53:17 -0800

    changed ip addresses

--- main_script.py
+++ main_script.py
@@ -23,8 +23,8 @@ from typing import Optional
 # ==============================
 cmd = "govee2mqtt/target/debug/govee serve --govee-email hebardiansbehardians@gmail.com --govee-password 777Markofthebeast!"
 interval = 600  # seconds between readings
-plug_ip = "192.168.0.12"   # Kasa plug controlling solar heater pump
-heater_ip = "192.168.0.14"  # Kasa plug controlling hot tub heater
+plug_ip = "192.168.0.65"   # Kasa plug controlling solar heater pump
+heater_ip = "192.168.0.64"  # Kasa plug controlling hot tub heater
 
 HOT_TUB_MARKER = 'Hot Tub Thermometer\\'
 HOT_TUB_OFFSET = 159
